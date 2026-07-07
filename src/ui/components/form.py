1579d1d9b66069c9e6f430fdff807abffb2af3d6
VlexandreMG <dodolashmou@gmail.com>
2026-07-07 20:21:31 +0300

    Input

--- src/ui/components/form.py
+++ src/ui/components/form.py
@@ -6,10 +6,13 @@ class Form:
         self.parent = parent
 
         self.cadre = tk.LabelFrame(self.parent, text=" Nouvel Appareil ", padx=10, pady=10)
-        self.cadre.pack(fill="x", padx=15, pady=10)
+        self.cadre.pack(padx=15, pady=10)
 
         self.creer_form()
 
     def creer_form(self):
         nomApp = tk.Label(self.cadre, text="Nom de l'appareil")
         nomApp.pack(side="left", padx=5)
+
+        self.entry_nom = tk.Entry(self.cadre, width=20)
+        self.entry_nom.pack(side="left", padx=5)
